5ee7b55a4870d220ea6b920948087d5e7503e204
jolicht <johannes@lichtenwallner.at>
2020-11-18 07:34:05 +0100

    using phpspec

---
+++ src/User.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+final class User
+{
+    private string $name;
+
+    private string $email;
+
+    public function __construct(string $name, string $email)
+    {
+        $this->name  = $name;
+        $this->email = $email;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+}
