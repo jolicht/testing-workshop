8982239b4a73f29e476120673bdf74625d26a77a
jolicht <johannes@lichtenwallner.at>
2020-11-19 15:06:11 +0100

    added tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -12,6 +12,13 @@ use PHPUnit\Framework\TestCase;
  */
 class UserTest extends TestCase
 {
+    private User $user;
+    
+    protected function setUp(): void
+    {
+        $this->user = User::create('testName', 'test@test.at');
+    }
+    
     /**
      * @test
      */
@@ -19,13 +26,38 @@ class UserTest extends TestCase
     {
         $this->assertInstanceOf(User::class, User::create('testName', 'test@test.at'));
     }
-    
+
     /**
      * @test
      */
-    public function it_converts_user_to_string()
+    public function it_gets_name()
     {
-        $user = User::create('testName', 'test@test.at');
-        $this->assertSame('inactive user testName', (string) $user);
+        $this->assertSame('testName', $this->user->getName());
     }
+
+    /**
+     * @test
+     */
+    public function it_gets_email()
+    {
+        $this->assertSame('test@test.at', $this->user->getEmail());
+    }
+
+    /**
+     * @test
+     */
+    public function it_converts_inactive_user_to_string()
+    {
+        $this->assertSame('inactive user testName', (string) $this->user);
+    }
+
+    /**
+     * @test
+     */
+    public function it_converts_active_user_to_string()
+    {
+        $this->user->activate();
+        $this->assertSame('active user testName', (string) $this->user);
+    }
+
 }
